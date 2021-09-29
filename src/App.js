5437afafb5149bcdb0672c9ae52fcaadcfe78775
Jose  Neto <dev.joseneto@gmail.com>
2021-09-28 22:09:18 -0400

    hello world adjusted.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellow World
+          Hello World
         </p>
         <a
           className="App-link"
