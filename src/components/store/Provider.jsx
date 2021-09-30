53e3896408d7513a0c66e6217ae5ca91095aeb4d
Jose  Neto <dev.joseneto@gmail.com>
2021-09-30 07:30:51 -0400

    implemented Context.ja => (Context.api) and ultilities, userSotorage and storage.

---
+++ src/components/store/Provider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Context from './Context'
+
+const StoreProvider = ({ children }) => {
+
+    return (
+      <Context.Provider
+        value={{
+          token,
+          setToken
+        }}
+      >
+         {children}
+      </Context.Provider>   
+    )
+}
+
+export default StoreProvider
